5c8786478585e31d60ed9f9e7f969f436ad1aae8
遂 李 <lisui0807@hotmail.com>
2015-09-06 14:41:21 +0800

    2

---
+++ CountAndSay.swift
@@ -0,0 +1,55 @@
+//
+//  CountAndSya.swift
+//  LeetCode
+//
+//  Created by 遂 李 on 7/26/15.
+//  Copyright (c) 2015 遂 李. All rights reserved.
+//
+
+import Foundation
+
+class CountAndSay{
+    
+    
+    class func getTheNthString(number : Int) -> String{
+        
+        var result : String = String("1")
+        
+        for _ in 1...number{
+            
+            result = getNextString(result)
+            
+        }
+        
+        return result
+        
+    }
+    
+    class func getNextString(currStr : String) -> String{
+        
+        var previousChar : Character = currStr[0]
+        var repeatCount : Int = 1
+        var newString  = String()
+        
+        for var i = 1 ; i < count(currStr) ; ++i {
+            
+            if previousChar == currStr[i] {
+                ++repeatCount
+            }else{
+                
+                //newString.append(Character(UnicodeScalar(repeatCount)))
+                newString += "\(repeatCount)"
+                newString.append(previousChar)
+                previousChar = currStr[i]
+                repeatCount = 1
+            }
+        }
+        
+        newString += "\(repeatCount)"
+        
+        newString.append(previousChar)
+        
+        return newString
+    }
+    
+}
