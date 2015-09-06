33a9a17118175da86851c17d038ad56afc4b307e
遂 李 <lisui0807@hotmail.com>
2015-09-06 14:40:37 +0800

    first file

---
+++ BinaryAdd.swift
@@ -0,0 +1,70 @@
+//
+//  BinaryAdd.swift
+//  LeetCode
+//
+//  Created by 遂 李 on 8/3/15.
+//  Copyright (c) 2015 遂 李. All rights reserved.
+//
+
+import Foundation
+
+class BinaryAdd {
+    
+    class func add( a : String, b : String ) -> String{
+        
+        let len1 : Int = count(a)
+        
+        let len2  : Int = count(b)
+        
+        let maxLen = len1 > len2 ? len1 : len2
+        
+        var result = String()
+        
+        var needStep = false
+        
+        for var currIndex : Int = 0 ; currIndex < maxLen ; ++currIndex {
+            
+            var currValue = 0
+            
+//            if (2 < 4) && (a[currIndex] == 1) {
+//                currValue++
+//            }
+            
+            if (currIndex < len1) && a[currIndex] == "1" {
+                currValue++
+            }
+            
+            
+            if currIndex < len2 && b[currIndex] == "1" {
+                currValue++
+            }
+            
+            if needStep {
+                currValue++
+            }
+            
+            switch(currValue){
+            case 0:
+                result = "0" + result
+                needStep = false
+            case 1:
+                result = "1" + result
+                needStep = false
+            case 2:
+                result = "0" + result
+                needStep = true
+            case 3:
+                result = "1" + result
+                needStep = true
+            default:
+                assertionFailure("this currValue should not happend")
+            }
+        }
+        
+        if needStep {
+            result = "1" + result
+        }
+        return result
+        
+    }
+}
